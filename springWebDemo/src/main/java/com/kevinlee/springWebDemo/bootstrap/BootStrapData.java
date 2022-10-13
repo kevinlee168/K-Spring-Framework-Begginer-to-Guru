e4950126388cd451d8473baebbe187a07a98b46c
kevinlee168 <leezk007@leezks-MacBook-Pro.local>
2022-10-13 11:47:15 +0800

    add Relationship of publisher and books

--- springWebDemo/src/main/java/com/kevinlee/springWebDemo/bootstrap/BootStrapData.java
+++ springWebDemo/src/main/java/com/kevinlee/springWebDemo/bootstrap/BootStrapData.java
@@ -23,14 +23,27 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Start to add some authors and books into the DB....");
 
+        System.out.println("Start to add publishers...");
+        Publisher publisher = new Publisher();
+        publisher.setName("ABC Publishing");
+        publisher.setCity("New York");
+        publisher.setState("NY");
+        publisherRepository.save(publisher);
+
+        System.out.println("Added " + publisherRepository.count() + "publisher into the DB.");
+
+        System.out.println("Start to add some authors and books into the DB....");
         Author kevin = new Author("Kevin", "Lee");
         Book book = new Book("The Power of Speaking", "16816857568");
+        book.setPublisher(publisher);
+
         kevin.getBooks().add(book);
         book.getAuthors().add(kevin);
         authorRepository.save(kevin);
         bookRepository.save(book);
+        publisher.getBooks().add(book);
+        publisherRepository.save(publisher);
 
         Author luice = new Author("Luice", "Rod");
         Book noEJB = new Book("J2EE Development without EJB", "12345657345");
@@ -42,13 +55,7 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println("Added " + bookRepository.count() + " books and "
                 + authorRepository.count() + " authors into the DB...");
 
-        System.out.println("Start to add publishers...");
-        Publisher publisher = new Publisher();
-        publisher.setName("ABC Publishing");
-        publisher.setCity("New York");
-        publisher.setState("NY");
-        publisherRepository.save(publisher);
-
-        System.out.println("Added " + publisherRepository.count() + " into the DB.");
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
     }
 }
